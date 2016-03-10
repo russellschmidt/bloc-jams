44c04df3c273da867279b20439be1bcdc75c24a0
Russell Schmidt <russell.schmidt@me.com>
2016-03-09 22:42:25 -0800

    make landing page logo rotate

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,28 +1,30 @@
-//var animatePoints = function() {
-  var points = document.getElementsByClassName('point');
-  
-  var revealFirstPoint = function() {
-    points[0].style.opacity = 1;
-    points[0].style.transform = "scaleX(1) translateY(0)";
-    points[0].style.msTransform = "scaleX(1) translateY(0)";
-    points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
-  
-  var revealSecondPoint = function() {
-    points[1].style.opacity = 1;
-    points[1].style.transform = "scaleX(1) translateY(0)";
-    points[1].style.msTransform = "scaleX(1) translateY(0)";
-    points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
-  
-  var revealThirdPoint = function() {
-    points[2].style.opacity = 1;
-    points[2].style.transform = "scaleX(1) translateY(0)";
-    points[2].style.msTransform = "scaleX(1) translateY(0)";
-    points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
-  
-  revealFirstPoint();
-  revealSecondPoint();
-  revealThirdPoint();
-//};
+var points = document.getElementsByClassName('point');
+
+var degree = 0;
+
+var revealPoint = function(points) {
+  for (var i = 0; i < points.length; i++) {
+    points[i].style.opacity = 1;
+    points[i].style.transform = "scaleX(1) translateY(0)";
+    points[i].style.msTransform = "scaleX(1) translateY(0)";
+    points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+  }
+};
+
+
+function rotateImage(logoImage, deg) {
+  logoImage.style.transform = "rotate("+deg+"deg)";
+  logoImage.style.msTransform = "rotate("+deg+"deg)";
+  logoImage.style.WebkitTransform = "rotate("+deg+"deg)";
+}
+
+revealPoint(points);
+
+window.setInterval(function(){
+  var logoImage = document.getElementById('logo-image');
+  rotateImage(logoImage, degree);
+  degree++;
+  if (degree === 360) {
+    degree = 1;
+  }
+}, 33);
